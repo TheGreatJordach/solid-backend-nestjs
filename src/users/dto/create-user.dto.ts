91de0d44ee8d787ef43d88c8fd41268b9c870d4a
Jordach Makaya H.M <4thinkai@gmail.com>
2024-08-10 16:29:10 +0100

    creating User entity

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+  readonly name: string;
+  readonly email: number;
+  readonly phone: number;
+  readonly password: number;
+}
---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,7 @@
+export class User {
+  id: number;
+  name: string;
+  email: number;
+  phone: number;
+  password: number;
+}
